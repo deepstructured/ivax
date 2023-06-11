9302c954039e3c2a3e3d9ab35343f818c3e6e0b1
Ilya Minkin <shadowpounce@icloud.com>
2023-06-11 16:04:27 +0300

    up

--- src/widgets/Preloader/Preloader.tsx
+++ src/widgets/Preloader/Preloader.tsx
@@ -8,6 +8,7 @@ import { Store } from '../../app/providers/store'
 export const Preloader = () => {
   const { pageLoaded, setPageLoaded } = useContext(Store)
   const [active, setActive] = useState<boolean>(true)
+  const [imageLoaded, setImagedLoaded] = useState<boolean>(false)
 
   return (
     <div className={clsx(styles.preloader, !active && styles.hidden)}>
@@ -17,37 +18,46 @@ export const Preloader = () => {
       <div className="bulbWrapper">
         <div className="shadow"></div>
         <div className="content">
-          <Player
-            className="wavingBubbles"
-            autoplay
-            speed={1.25}
-            onEvent={(event) => {
-              if (event === 'complete') {
-                setActive(false)
-                setTimeout(
-                  () => setPageLoaded(true),
-                  window.innerWidth > 768 ? 1250 : 1050
-                )
-              }
-            }}
-            src="/animations/preloader-bubbles.json"
-          ></Player>
+          {imageLoaded && (
+            <>
+              <Player
+                className="wavingBubbles"
+                autoplay
+                speed={1.25}
+                onEvent={(event) => {
+                  if (event === 'complete') {
+                    setActive(false)
+                    setTimeout(
+                      () => setPageLoaded(true),
+                      window.innerWidth > 768 ? 1250 : 1050
+                    )
+                  }
+                }}
+                src="/animations/preloader-bubbles.json"
+              ></Player>
 
-          <Player
-            className="innerBubbles"
-            autoplay
-            loop
-            src="/animations/preloader-inner-bubbles.json"
-          ></Player>
-          {window.innerWidth > 565 && (
-            <Player
-              className="innerBubbles bubblesCopy"
-              autoplay
-              loop
-              src="/animations/preloader-inner-bubbles.json"
-            ></Player>
+              <Player
+                className="innerBubbles"
+                autoplay
+                loop
+                src="/animations/preloader-inner-bubbles.json"
+              ></Player>
+              {window.innerWidth > 565 && (
+                <Player
+                  className="innerBubbles bubblesCopy"
+                  autoplay
+                  loop
+                  src="/animations/preloader-inner-bubbles.json"
+                ></Player>
+              )}
+            </>
           )}
-          <img className="bulb" src="/images/bulb-hero.png" alt="" />
+          <img
+            onLoad={() => setImagedLoaded(true)}
+            className="bulb"
+            src="/images/bulb-hero.png"
+            alt=""
+          />
         </div>
       </div>
     </div>
